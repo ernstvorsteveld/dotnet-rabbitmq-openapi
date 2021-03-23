037326fb3bb0c1a9b9eadb7d9c262a3741f53a16
Ernst Vorsteveld <ernst.vorsteveld@gmail.com>
2021-03-23 13:58:41 +0100

    added start of a mongo piece

---
+++ MongoDB/Customer.cs
@@ -0,0 +1,18 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDB
+{
+    public class Customer
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [BsonElement("PhoneNumber")]
+        public string Contact { get; set; }
+        public string Email { get; set; }
+    }
+
+}
---
+++ MongoDB/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "mongodb://root:root@localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("test");
+            var collection = database.GetCollection<Customer>("uesr");
+
+            var customer = new Customer();
+            customer.LastName = "Boer";
+            collection.InsertOne(customer);
+        }
+    }
+}
